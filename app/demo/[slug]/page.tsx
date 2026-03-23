4df4188a9ceedbe82f31061de9a9a2b7f3d80458
Shri-Willz <shriwllz@gmail.com>
2026-03-23 19:50:01 +0530

    fix:removed vapiconfig

--- app/demo/[slug]/page.tsx
+++ app/demo/[slug]/page.tsx
@@ -1,8 +1,20 @@
 'use client'
 
 import VapiWidget from "../../../components/Vapi-widget";
-import { vapiconfi } from "../../../components/vapiconfig";
 import { usePathname } from "next/navigation";
+
+
+const  vapiconfi ={
+    vapi_api_key: process.env.NEXT_PUBLIC_VAPI_KEY || "",
+
+    assistants: {
+        "/demo/dental" : process.env.NEXT_PUBLIC_DENTAL_AGENT || "",
+        "/demo/consulting": process.env.NEXT_PUBLIC_CONSULTING_AGENT || "",
+        "/demo/homeServices": process.env.NEXT_PUBLIC_HOME_SERVICES_AGENT || "",
+        "/demo/realEstate": process.env.NEXT_PUBLIC_REAL_ESTATE_AGENT || "",
+        "/demo/general": process.env.NEXT_PUBLIC_GENERAL_AGENT || ""
+    }
+}
 
 export default function DemoNiche(){
     const pathname: string = usePathname()
